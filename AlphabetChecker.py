4892c989ba3ff1e4b056123c9c57bda6436a456e
Mini-Electro <scott.white0703@gmail.com>
2025-11-26 10:13:50 +0200

    Add files via upload

---
+++ AlphabetChecker.py
@@ -0,0 +1,6 @@
+ch = input("Enter any character: ")
+
+if (ch>= "a" and ch<= "z") or (ch>= "A" and ch<= "Z"):
+    print("Letter is in the alphabet")
+else:
+    print("This character is not in the alphabet")
