e45caba734684cacc4191e0229a0a3eba2504ba4
Mini-Electro <scott.white0703@gmail.com>
2025-12-03 10:19:11 +0200

    Add files via upload

---
+++ AgeChecker.py
@@ -0,0 +1,9 @@
+age = int(input("Enter your age: "))
+
+if age > 10:
+    if age<20:
+        print("age is between 10 and 20.")
+    else:
+        print("Age is greater than 10 but not less than 20")
+else:
+    print("Age is not greater than 10")
