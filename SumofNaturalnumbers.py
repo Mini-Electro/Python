2461103c0581be37e7626427261ce6907ee62733
Mini-Electro <scott.white0703@gmail.com>
2025-12-08 09:48:59 +0200

    Add files via upload

---
+++ SumofNaturalnumbers.py
@@ -0,0 +1,15 @@
+#Input the value of terms
+n = int(input("Enter the value of terms: "))
+
+sum = 0 #initalise
+i = 1 #initalise
+while i<=n: #loop will run from 1 to n
+    sum + i
+    i = i+1
+
+print("\nSum =", sum)
+
+
+i = 0
+while i<=0:
+    print("i will run forever") 
