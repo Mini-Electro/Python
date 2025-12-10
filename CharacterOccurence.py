d81ebc6b3ad8a5c0d5fd87925b7e79be1a014d48
Mini-Electro <scott.white0703@gmail.com>
2025-12-10 10:05:49 +0200

    Add files via upload

---
+++ CharacterOccurence.py
@@ -0,0 +1,20 @@
+# Take input of a word
+string = input("Please Enter your own word: ")
+
+# take input of a character
+char = input("Please enter your own character: ")
+
+i = 0
+count = 0
+# loop will to find the occurence of a character
+
+while (i < len(string)):
+
+
+
+    if (string[i] == char): # condition 1
+        count = count + 1
+    i = i+1
+
+# Display the result
+print("The total number of times", char, "has occured =", count)
