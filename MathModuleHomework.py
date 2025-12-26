26936b4c4751bf9a630dbc14c32a3f50abd6e4b6
Mini-Electro <scott.white0703@gmail.com>
2025-12-26 10:16:57 +0200

    Add files via upload

---
+++ MathModuleHomework.py
@@ -0,0 +1,11 @@
+import math
+
+num = 25
+print("Sqaure root:", math.sqrt(num))
+print("Factorial:", math.factorial(5))
+print("The absolute value of -10:", abs(-10))
+print("Two to the power of three:", pow(2,3))
+print("4.567 rounded up to two decimal places:", round(4.567, 2))
+print("The highest value:", max(3,7,1))
+print("The lowest value:", min(3,7,1))
+print("The sum of these three:", sum([1,2,3]))
