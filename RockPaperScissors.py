b9bc53e4282f66925a85aebffefd1d1a360a9265
Mini-Electro <scott.white0703@gmail.com>
2026-03-12 21:35:11 +0200

    Add files via upload

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,34 +1,40 @@
 import random
+from colorama import Fore, Style, init
 
+init(autoreset=True)
+
+possible_actions = ["rock", "paper", "scissors"]
 
 while True:
-    user_action = input("Enter a choice! (rock, paper, scissors): ")
-    possible_actions = ["rock", "paper", "scissors"]
-    #using random function
+    user_action = input(Fore.CYAN + "Enter a choice! (rock, paper, scissors): ").lower()
     computer_action = random.choice(possible_actions)
-    print(f"\nYou Chose {user_action}, computer chose {computer_action}.\n")
 
-
-
+    print(
+        Fore.YELLOW
+        + f"\nYou chose {user_action}, computer chose {computer_action}.\n"
+    )
 
     if user_action == computer_action:
-        print(f"Both players selected {user_action}. Its a tie!")
+        print(Fore.BLUE + f"Both players selected {user_action}. It's a tie!")
     elif user_action == "rock":
         if computer_action == "scissors":
-            print("Rock smashes scissors! You win!")
+            print(Fore.GREEN + "Rock smashes scissors! You win!")
         else:
-            print("Paper covers rock! You lose.")
+            print(Fore.RED + "Paper covers rock! You lose.")
     elif user_action == "paper":
         if computer_action == "rock":
-            print("Paper covers rock. You win!")
+            print(Fore.GREEN + "Paper covers rock! You win!")
         else:
-            print("Scissors cuts paper!, you lose.")
+            print(Fore.RED + "Scissors cut paper! You lose.")
     elif user_action == "scissors":
         if computer_action == "paper":
-            print("Scissors cuts paper! You win!")
+            print(Fore.GREEN + "Scissors cut paper! You win!")
         else:
-            print("Rock smashes scissors! You lose..")
-# take input for playing again
-    play_again = input("Play again? (y/n): ")
+            print(Fore.RED + "Rock smashes scissors! You lose.")
+    else:
+        print(Fore.MAGENTA + "That is not a valid choice.")
+
+    play_again = input(Fore.CYAN + "Play again? (y/n): ").lower()
     if play_again != "y":
+        print(Fore.WHITE + "Thanks for playing!")
         break
